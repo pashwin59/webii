4d8c42e836debfd4ddab76ed0b9d97be3cfc8fcc
paswin59 <poudelaswin942@gmail.com>
2026-01-21 12:05:23 +0545

    objects

---
+++ object2.js
@@ -0,0 +1,10 @@
+let person = {
+    name: "ss",
+    age: 22,
+    isStudent: true
+};
+
+// Access object properties
+console.log(person.name);      
+console.log(person.age);       // 
+console.log(person.isStudent);
